1c25a1a7a7cca86482066ffb26c1687bd5ab85d3
raoshah <shahrukhrao1@gmail.com>
2025-04-11 08:56:48 +0530

    added frontend & backend

---
+++ backend/aiapp/views.py
@@ -0,0 +1,15 @@
+from django.http import JsonResponse
+
+
+def index(request):
+    
+    data = {
+    'message': "done",
+    'status': 'success',
+    'items': [
+        {'id': 1, 'name': 'Item 1'},
+        {'id': 2, 'name': 'Item 2'},
+        {'id': 3, 'name': 'Item 3'},
+    ]
+    }
+    return JsonResponse(data)
