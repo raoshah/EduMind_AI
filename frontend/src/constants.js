8b23643b14d90b1ffa7efc75a8b5a8655583695d
raoshah <shahrukhrao1@gmail.com>
2025-04-17 17:18:37 +0530

    Apr17

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://0.0.0.0:8001"
+export const API_URL = "https://raoshahrukh.pythonanywhere.com"
 // export const API_URL = "https://edumindai.vercel.app"
 // export const API_URL = "http://127.0.0.1:8000"
